b86d5388687cc2034ae77c5d72dc40e317c8c4a2
Simon de Haan <simon@praekeltfoundation.org>
2014-01-03 14:24:05 +0200

    adding travis

--- run-tests.sh
+++ run-tests.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-virtualenv --no-site-packages ve && \
-source ve/bin/activate && \
-pip install -r requirements.pip && \
-deactivate
-source ve/bin/activate && \
 nosetests --with-doctest --with-coverage --cover-package=smpp --with-xunit && \
-coverage xml smpp/* && \
-pep8 --repeat --exclude '0*.py' smpp > pep8.txt && \
-deactivate
+coverage xml smpp/*
